7349a93a85d71c512787241a923dc7a6ae43049f
MackyClap <martimac2320@gmail.com>
2022-11-11 19:59:43 -0500

    update users seed file with data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO 
+  users (first_name,last_name,email,phone) 
+VALUES 
+  ('Alice', 'Mckenzie', 'amack@gmail.com', 4163335421),
+  ('Jack', 'Sparrow', 'jspar@gmail.com', 4163332451),
+  ('Jonny', 'Walker', 'jwalker@gmail.com', 4163335498);
