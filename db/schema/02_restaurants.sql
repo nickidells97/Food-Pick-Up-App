bf7965a92da3ad543be890090860f086b6e696e5
Nick Dellar <ndelfern@live.ca>
2022-11-12 19:54:16 +0000

    re-add seperate schema files for db

---
+++ db/schema/02_restaurants.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS restaurants CASCADE;
+
+CREATE TABLE restaurants (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name CHAR(50) NOT NULL, -- Added name
+  email CHAR (50) NOT NULL, -- Added email
+  phone_number INTEGER NOT NULL -- Removed status column as this was a stretch feature
+);
---
+++ db/schema/04_menu_items.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS menu_items CASCADE;
+
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  item_name CHAR(50) NOT NULL,
+  item_price INT NOT NULL,
+  item_description TEXT NOT NULL,
+  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE
+);
