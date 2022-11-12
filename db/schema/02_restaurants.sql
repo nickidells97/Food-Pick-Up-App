57f34613b7099277eae7fb69998c5618ce4bef99
Nick Dellar <ndelfern@live.ca>
2022-11-12 18:21:17 +0000

    Delete old schema files and fix 01_schema

--- db/schema/02_restaurants.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS restaurants CASCADE;
-
-CREATE TABLE restaurants (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name CHAR(50) NOT NULL, -- Added name
-  email CHAR (50) NOT NULL, -- Added email
-  phone_number INTEGER NOT NULL -- Removed status column as this was a stretch feature
-);
--- db/schema/06_menu_items.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS menu_items CASCADE;
-
-CREATE TABLE menu_items (
-  id SERIAL PRIMARY KEY NOT NULL,
-  item_name CHAR(50) NOT NULL,
-  item_price INT NOT NULL,
-  item_description TEXT NOT NULL,
-  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE
-);
