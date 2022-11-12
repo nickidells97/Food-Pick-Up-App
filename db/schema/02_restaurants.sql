a240ce57985ad2a4ec214d560d023b3641b467e8
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-11 20:33:07 -0500

    Update column type to be consist with ERD

--- db/schema/02_restaurants.sql
+++ db/schema/02_restaurants.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS restaurants CASCADE;
 
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL, -- Added name
-  email VARCHAR (50) NOT NULL, -- Added email
-  phone_number SMALLINT NOT NULL -- Removed status column as this was a stretch feature
+  name CHAR(50) NOT NULL, -- Added name
+  email CHAR (50) NOT NULL, -- Added email
+  phone_number INTEGER NOT NULL -- Removed status column as this was a stretch feature
 );
