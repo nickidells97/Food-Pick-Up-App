a027eeee27750878594a14b0bc0cf40056fff017
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-13 23:46:09 -0500

    Add queries to pull db data

---
+++ db/queries/03_select_orders.sql
@@ -0,0 +1,5 @@
+SELECT order_items.order_id, users.first_name as user_name, users.email as email, users.phone as phone , menu_items.item_name as name, menu_items.item_price
+  FROM order_items
+  JOIN menu_items ON menu_items.id = menu_item_id
+  JOIN orders ON orders.id = order_items.order_id
+  JOIN users ON users.id = orders.user_id;
