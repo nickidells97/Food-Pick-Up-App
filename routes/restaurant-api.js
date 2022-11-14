237ea401b546798352ca3662d035052edd955d30
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-13 23:43:23 -0500

    Add route which pushes db data to webpage

---
+++ routes/restaurant-api.js
@@ -0,0 +1,27 @@
+const express = require('express'); // Imports express package
+const router  = express.Router(); // Creates subroutes for /restaurants
+const db = require('../db/connection');
+
+
+router.get("/", (req,res) => {
+  
+  let selectOrders = `
+  SELECT order_items.order_id, users.first_name as user_name, users.email as email, users.phone as phone , menu_items.item_name as name, menu_items.item_price
+    FROM order_items
+    JOIN menu_items ON menu_items.id = menu_item_id
+    JOIN orders ON orders.id = order_items.order_id
+    JOIN users ON users.id = orders.user_id
+    WHERE order_items.order_id = 1;
+  `;
+  
+  db.query(selectOrders)
+    .then(data => {
+      const orders = data.rows[0];
+      res.json(orders);
+    })
+  
+
+});
+
+
+module.exports = router; // Allows subroutes to be accessed
