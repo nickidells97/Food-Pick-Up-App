4a07c957dfb13c2c4891a4a6a5e473b6859f98af
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-14 22:28:17 -0500

    Update render function to load multiple order cards

--- restaurants/scripts/restaurants.js
+++ restaurants/scripts/restaurants.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 
 
-
  const ordersCardContructor = (obj)=> {
 
   let orders = `
     <section class = "user-orders">
+    <div id="target"> Click Here</div>
       <div class = "order-card-header">
         <div class = "order-acceptance">
-          <form>
-            <button class = "accept">Accept</button>
-            <button class = "reject">Reject</button>
-          </form>
-        </div>
-        <div class = "order-duration">20</div>
-        </div>
+          <form>   
+          <div class="order-status"> 
+            <input type="submit" name="accept" value="Accept" class="order-status">
+            <input type="submit" name="decline" value="Reject">
+            <input type="submit" name="completed" value="Completed">
+          </div>
+            </form>
         <div class = "order-details">
         <h5>Order Details</h5>  
         <p> Customer name: ${obj.user_name}</p>
           <p>Order Date: 11/13/2022</p>
           <p> Order Item: ${obj.name}</p>
-          <p>Total Bill: ${obj.item_price}</p>
+          <p id ="total">Total: ${obj.item_price}</p>
           <h5>Customer Contact Details</h5>
           <p>Email: ${obj.email}</p>
           <p>Phone: ${obj.phone}</p>
@@ -32,19 +32,38 @@ $(document).ready(function() {
   };
 
   const renderOrderCards = (data)=> {
-  let orders = ordersCardContructor(data);
-
-  $('#restaurant-orders').append(orders);
+    data.forEach((x)=> {
+      let orders = ordersCardContructor(x);
+      $('#restaurant-orders').append(orders);
+    });
+    
   };
-
+  
   const loadOrders = function() {
-    $.ajax("/restaurants/routes/data",{method: "GET"})
-      .then(function(data) {
-        // console.log(data);
-        renderOrderCards(data);
+    $.ajax("/restaurants/orders",{method: "GET"})
+    .then(function(data) {
+      // console.log(data);
+      renderOrderCards(data);
+      
+      // Attach listener
+      $( "#target" ).on( "click", function() {
+        alert( $( this ).text() );
+      });
+      
+
+
       });
   };
 
   loadOrders();
+
+
+  $("#total").click((event)=> {
+    console.log("I was clicked");
+
+    event.preventDefault(); // Prevent default action on button click
+  });
+
+
 
 });
