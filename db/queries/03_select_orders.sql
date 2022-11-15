4cfcc1249b979b9e5e3c210596f248f3946ee2f8
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-14 22:35:47 -0500

    Add duration column to query

--- db/queries/03_select_orders.sql
+++ db/queries/03_select_orders.sql
@@ -1,4 +1,4 @@
-SELECT order_items.order_id, users.first_name as user_name, users.email as email, users.phone as phone , menu_items.item_name as name, menu_items.item_price
+SELECT order_items.order_id, users.first_name as user_name, users.email as email, users.phone as phone , menu_items.item_name as name, menu_items.item_price, menu_items.order_duration as duration
   FROM order_items
   JOIN menu_items ON menu_items.id = menu_item_id
   JOIN orders ON orders.id = order_items.order_id
