d0bc1c174aed19b195510fff6ec689a2ee78b304
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-14 22:34:10 -0500

    Add order duration to seed

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,6 +1,6 @@
 INSERT INTO menu_items (item_name, item_price, item_description, restaurant_id)
-  VALUES ('Cheeseburger', 14, 'A delicious meat patty covered in cheese between two buns.', 1),
-('Club B.L.T', 10, 'Chicken, bacon, lettuce and tomato sandwich served with salad.', 1),
-('Chicken Strips', 14, 'Tender fried chicken strips served with dipping sauce of your choice.', 1),
-('Caesar Salad', 14, 'Classic Caesar salad with dressing, parmesan and bacon bits.', 1),
-('Grilled Cheese', 8, 'Grilled cheese sandwich served with our classic fries.', 1);
+  VALUES ('Cheeseburger', 14, 'A delicious meat patty covered in cheese between two buns.', 15, 1),
+('Club B.L.T', 10, 'Chicken, bacon, lettuce and tomato sandwich served with salad.', 20, 1),
+('Chicken Strips', 14, 'Tender fried chicken strips served with dipping sauce of your choice.', 10, 1),
+('Caesar Salad', 14, 'Classic Caesar salad with dressing, parmesan and bacon bits.', 5, 1),
+('Grilled Cheese', 8, 'Grilled cheese sandwich served with our classic fries.', 10, 1);
