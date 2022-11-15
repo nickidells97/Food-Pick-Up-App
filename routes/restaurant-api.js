131d12d7084b06866e93ebf73f9d983be73e6a31
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-14 22:44:15 -0500

    Update query to include duration column

--- routes/restaurant-api.js
+++ routes/restaurant-api.js
@@ -6,7 +6,7 @@ const db = require('../db/connection');
 router.get("/orders", (req,res) => {
   
   let selectOrders = `
-  SELECT order_items.order_id, users.first_name as user_name, users.email as email, users.phone as phone , menu_items.item_name as name, menu_items.item_price
+  SELECT order_items.order_id, users.first_name as user_name, users.email as email, users.phone as phone , menu_items.item_name as name, menu_items.item_price, menu_items.order_duration as duration
     FROM order_items
     JOIN menu_items ON menu_items.id = menu_item_id
     JOIN orders ON orders.id = order_items.order_id
