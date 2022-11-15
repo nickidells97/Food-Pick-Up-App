7eb14f304aa5ec1567b2f2e2e3d786f8b4609141
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-14 22:43:17 -0500

    Add update missing column to query syntax

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,4 +1,4 @@
-INSERT INTO menu_items (item_name, item_price, item_description, restaurant_id)
+INSERT INTO menu_items (item_name, item_price, item_description, order_duration, restaurant_id)
   VALUES ('Cheeseburger', 14, 'A delicious meat patty covered in cheese between two buns.', 15, 1),
 ('Club B.L.T', 10, 'Chicken, bacon, lettuce and tomato sandwich served with salad.', 20, 1),
 ('Chicken Strips', 14, 'Tender fried chicken strips served with dipping sauce of your choice.', 10, 1),
