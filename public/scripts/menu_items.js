ba63a1fef12f12412057e3e88087f2a4904006c7
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-16 23:56:03 -0500

    Add structure to html for styling

--- public/scripts/menu_items.js
+++ public/scripts/menu_items.js
@@ -4,15 +4,21 @@ $(document).ready(() => {
   const createMenuItem = (obj) => {
 
     const menu =  `
-    <form method="POST" action="/usercart">
-            <div>
-              <h5> ${obj.item_name}!</h1>
-      <img src="https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" width="200" height="200">
-      <span> ${obj.item_description} </span>
-      <span> Price: ${obj.item_price} </span>
+    <div class ="menu-items">
+      <form method="POST" action="/usercart">
+        <div class="menu-container">
+        <img class ="menu-display" src="https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" width="125" height="125">
+          <div class="menu-info">
+            <h5 class="menu-title"> ${obj.item_name}!</h1>
+            <span> ${obj.item_description} </span>
+            <div class="info-footer">      
+              <span class="price"> Price: $${obj.item_price} </span>
               <button type="submit">Add to cart</button>
-            </div>
-          </form>
+            </div>     
+          </div>
+        </div>
+      </form>
+    </div>
     `;
     return menu
   };
